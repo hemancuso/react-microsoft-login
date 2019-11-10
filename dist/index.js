72e7dca242287801dda73587dcfa31ccd49619d9
Jeff Mancuso <jmancuso@expandrive.com>
2019-11-10 16:21:14 -0500

    update building

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MicrosoftLogin_1 = require("./MicrosoftLogin");
-exports.MicrosoftLogin = MicrosoftLogin_1.default;
-exports.default = MicrosoftLogin_1.default;
-//# sourceMappingURL=index.js.map
